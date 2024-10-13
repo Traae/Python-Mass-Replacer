53d0bd8f8cbf6abdfb468748932c947c78ec292a
Traae <traaebloxham@gmail.com>
2024-10-13 09:51:01 -0600

    Refactored some things, tried to find an elegant solution to the global variable problem, but for the size and function of this project, designing anything further isn't worth it, much to my frustration.

--- main.py
+++ main.py
@@ -5,42 +5,60 @@ import os
 from argparse import ArgumentParser
 
 
-def individual_process(change_list, to_search, replace_original):
-    print("Processing ", to_search)
-    original = open(to_search, 'r')
+def process_file(args, change_list, to_process):
+    original = open(to_process, 'r')
 
-    suffix = Path(to_search).suffix
-    to_write = to_search.replace(suffix, (' -Changed' + suffix))
-    changed_file = open(to_write, 'w')
+    if args.make | args.replace:
+        write = True
 
-    # Run it as many times as needed
-    for line in original.readlines():
-        # print("Now Changing: " + line)
-        for c in change_list.keys():
-            # print("Currently changing: " + c + " -> " + change_list.get(c))
-            pattern = re.compile(c)
-            line = re.sub(pattern, change_list.get(c), line)
-        changed_file.write(line)
+    if write:
+        suffix = Path(to_process).suffix
+        to_write = to_process.replace(suffix, (' -Changed' + suffix))
+        changed_file = open(to_write, 'w')
 
-    # Tie up the streams and replace the original file
+        # Run it as many times as needed
+        for line in to_process.readlines():
+            changed_file.write(make_changes(args, change_list, line))
+
+        # Tie up the streams and replace the original file
+        changed_file.close()
+        if args.Replace:
+            os.remove(to_process)
+            os.rename(to_write, to_process)
+
+    else:
+        for line in to_process.readlines():
+            print(make_changes(args, change_list, line))
+
     original.close()
-    changed_file.close()
-    if replace_original:
-        os.remove(to_search)
-        os.rename(to_write, to_search)
-
     return 0
 
 
-def recursive_process(dir, types, changes, replace_original):
-    for path in os.listdir(dir):
-        print("Working through Directory: ", path, '\n')
-        if Path(path).suffix in types:
-            individual_process(path, changes, replace_original)
+def make_changes(args, change_list, line):
+    changed = line
+    if args.LowerCase:
+        changed = line.lower()
+
+    for c in change_list.keys():
+        pattern = re.compile(c)
+        line = re.sub(pattern, change_list.get(c), changed)
+
+    return changed
+
+
+def recursive_process(path, to_scan):
+    for path in os.listdir(path):
+        if Path(path).suffix in to_scan:
+            process_file(path)
         elif Path.is_dir(path):
-            recursive_process(path, types, changes, replace_original)
-
+            recursive_process(path)
     return 0
+
+
+def process_dir(dir, to_scan):
+    for path in os.listdir(dir):
+        if Path(path).suffix in to_scan:
+            process_file(path)
 
 
 def add_dictionary(changes, dictionary):
@@ -61,7 +79,7 @@ def add_dictionary(changes, dictionary):
             dfile.close()
     except OSError as e:
         print("Error in trying to read: ", e)
-        #print("Error in trying to read: ", dictionary)
+        # print("Error in trying to read: ", dictionary)
 
 
 def setup_parser():
@@ -69,40 +87,47 @@ def setup_parser():
     process the input.
             Command list:
                 -e = make an example dictionary
-                -f = File to be searched to be followed by a File/path.txt
+                -f = File to be searched, to be followed by a File/path.txt
+                -s = String to processed, to be followed by "String for processing."
                 -d = read in a dictionary, to be followed by a dictionary/path.json.
+                -r = Replace, replace the original file.
+                -m = make a new file.
                 -b = Backward, reverse the replacement and target of the inputs
-                -r = Replace, replace the original file instead of making a new one.
+                -c = Change to be made, to be followed by "target:replacement"
+                -l = set the text to lower case before applying changes.
+                Set the path to start the recursive process in. Defaults to current working directory.
                 -a = All files, repeat replacement for all files in cwd
                 -s = All subdirectories, repeat for all files in subdirectories
-                -c = Change to be made, to be followed by "target:replacement"
+
     """
     parser = ArgumentParser(description='Find matches of a pattern in ' \
                                         'lines of file(s).', add_help=False)
     parser.add_argument('--help', action='help', help='show this help ' \
                                                       'message and exit')
-
     parser.add_argument('-e', '--Example', action='store_true',
                         help='Creates am example change dictionary in cwd.')
     parser.add_argument('-f', '--File', action='append',
                         help='File(s) to be processed.')
+    parser.add_argument('-s', '--String', action='append',
+                        help='String(s) to be processed and printed to terminal.')
     parser.add_argument('-d', '--Dictionary', action='append',
                         help='Dictionary of target:replacement pairs to be applied.')
     parser.add_argument('-r', '--Replace', action='store_true',
                         help='Delete and replace the original files, otherwise create changed copies.')
+    parser.add_argument('-m', '--Make', action='store_true',
+                        help='Make a new file containing the result.')
     parser.add_argument('-b', '--Backwards', action='store_true',
                         help='Reverse all of the target:replacements pairs in the process.')
     parser.add_argument('-c', '--Change', action='append',
-                        help='A "target:replacement" change to be applied.')
-    parser.add_argument('-a', '--All', action='store_true',
-                        help='Recursively process all files in all subdirectories in cwd.')
+                        help='A change to be applied, entered as: "target:replacement"')
+    parser.add_argument('-l', '--LowerCase', action='store_true',
+                        help='Set all text to lower case before applying changes.')
+    parser.add_argument('-r', '--Recursive', action='store_const',
+                        help='Input a specific path to start a recursive process in. Default = .txt only.')
+    parser.add_argument('-a', '--All', action='store_const',
+                        help='Input a specific directory to process all files in. Default = .txt only')
     parser.add_argument('-t', '--Type', action='append',
-                        help='Add a file type to scan when processing all files.')
-    # parser.add_argument('-p', '--Path', action='store_ture',
-    #                     help='Input a specific directory to start a recursive process in.')
-    # parser.add_argument('-i', '--ignore-case', action='store_true',
-    #                     help='case-insensitive search')
-
+                        help='Add a file type to scan when processing directories with -a or -r.')
     return parser
 
 
@@ -120,14 +145,16 @@ def make_example_dictionary():
         print("Example Dictionary made at: ", os.getcwd(), example)
     except OSError as e:
         print("Error in creating example dictionary: ", e)
-        #print("Error in creating example dictionary.")
+        # print("Error in creating example dictionary.")
 
 
 def main() -> int:
     parser = setup_parser()
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     change_list = dict()
+    to_scan = ['.txt']
+
     if args.Change is not None:
         for c in args.Change:
             pair = c.split(':')
@@ -150,27 +177,34 @@ def main() -> int:
             i += 1
         change_list = reverse_list
 
-    # Starting Report
-    if len(change_list) > 0:
-        print("Set to perform these changes: \n")
-        for c in change_list.items():
-            print(c)
-    if args.Replace:
-        print("Original files will be replaced.\n")
-    else:
-        print("New files will be created.\n")
-
+    # # Starting Report
+    # if len(change_list) > 0:
+    #     print("Set to perform these changes: \n")
+    #     for c in change_list.items():
+    #         print(c)
+    # if args.Replace:
+    #     print("Original files will be replaced.\n")
+    # else:
+    #     print("New files will be created.\n")
 
     # Begin Processing
-    if args.All is True:
-        print("Beginning Recursive Process.\n")
-        to_scan = ['.txt']
-        if args.Type is not None:
-            to_scan += args.Type
-        recursive_process(os.getcwd(), to_scan, change_list, args.Replace)
-    elif args.File is not None:
+    if args.Type is not None:
+        to_scan += args.Type
+
+    if args.All is not None:
+        process_dir(args.All, to_scan)
+
+    if args.Recursive is not None:
+        recursive_process(args.Recursive, to_scan)
+
+    if args.String is not None:
+        for s in args.String:
+            changed = make_changes(args, change_list, s)
+            print(s + " -> \n" + changed + "\n")
+
+    if args.File is not None:
         for f in args.File:
-            individual_process(change_list, f, args.Replace)
+            process_file(args, change_list, f)
 
     return 0
 
